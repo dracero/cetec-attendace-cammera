7dc0c8e6f4287b0064367aed426c09558a8bf374
dracero <diego.racero@gmail.com>
2022-08-21 16:08:02 -0300

    static

--- app.js
+++ app.js
@@ -21,8 +21,9 @@ var indexRouter = require('./routes/index');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+//app.set('views', path.join(__dirname, 'views'));
+//app.set('view engine', 'jade');
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.use(cors());
 app.use(logger('dev'));
