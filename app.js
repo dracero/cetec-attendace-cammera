a28591a96f3ef3b73d70226aeb58aa744728f6c1
dracero <diego.racero@gmail.com>
2022-08-21 18:48:12 -0300

    test

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 //app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'build')));
+//app.use(express.static(path.join(__dirname, 'build')));
 
 //Middleware sesssion persists in Mongo
 app.use(session({
