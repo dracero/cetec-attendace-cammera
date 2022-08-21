bd85d0ba582929e194c098ed65b7faaf54e5b4a3
dracero <diego.racero@gmail.com>
2022-08-21 18:22:50 -0300

    build

--- app.js
+++ app.js
@@ -21,16 +21,16 @@ var indexRouter = require('./routes/index');
 var app = express();
 
 // view engine setup
-//app.set('views', path.join(__dirname, 'views'));
-//app.set('view engine', 'jade');
-app.use(express.static(path.join(__dirname, 'build')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
 app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 //Middleware sesssion persists in Mongo
 app.use(session({
